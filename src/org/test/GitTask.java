ecefb9e5cc935d7fef23551e203b5eb69c86632f
nessiva <nessiva5@gmail.com>
2022-03-26 22:26:29 +0530

    Task Push

---
+++ src/org/test/GitTask.java
@@ -0,0 +1,30 @@
+package org.test;
+
+public class GitTask {
+	
+	private void gitPush() {
+	int a=10;
+	int a1=20;
+	int  c=a+a1;
+	
+		System.out.println("GITPUSH"+c);
+	}
+	
+	private void gitPull() {
+		int b = 20;
+		int b1 =20;
+		int c1=b-b1;
+		
+		System.out.println("GITPULLL"+c1);
+	}
+	
+	public static void main(String[] args) {
+		
+		GitTask git = new GitTask();
+		git.gitPush();
+		git.gitPull();
+		
+	}
+	
+
+}
